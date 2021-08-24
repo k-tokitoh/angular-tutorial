a5035e0681183723b15f6cb30e66804dff256fbc
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2021-08-24 12:15:07 +0900

    create 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'crisis-center', component: CrisisListComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
